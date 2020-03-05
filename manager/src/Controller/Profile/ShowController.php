40a07954eee5b97f95da722bb3736da8ed408228
Roman Slyusar <slyusarrv@yandex.ru>
2020-03-05 10:45:24 +0500

    Add email change

--- manager/src/Controller/Profile/ShowController.php
+++ manager/src/Controller/Profile/ShowController.php
@@ -2,19 +2,20 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Controller\Profile;
 
 use App\ReadModel\User\UserFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfileController extends AbstractController
+class ShowController extends AbstractController
 {
     private $users;
 
     public function __construct(UserFetcher $users)
     {
+
         $this->users = $users;
     }
 
@@ -24,8 +25,9 @@ class ProfileController extends AbstractController
      */
     public function show(): Response
     {
-        $user = $this->users->findDetail($this->getUser()->getId());
 
+
+        $user = $this->users->findDetail($this->getUser()->getId());
         return $this->render('app/profile/show.html.twig', compact('user'));
     }
 }
